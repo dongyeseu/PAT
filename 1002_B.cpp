402abd198a5557cd69505e4baa812a717517c729
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-07-06 14:41:28 +0800

    写出这个数 (20)-PAT乙级真题

--- 1002_B.cpp
+++ 1002_B.cpp
@@ -1,15 +1,15 @@
 /*
-1002 д������� (20)��20 �֣�
-����һ����Ȼ��n���������λ����֮�ͣ��ú���ƴ��д���͵�ÿһλ���֡�
+1002 写出这个数 (20)（20 分）
+读入一个自然数n，计算其各位数字之和，用汉语拼音写出和的每一位数字。
 
-�����ʽ��ÿ�������������1��������������������Ȼ��n��ֵ�����ﱣ֤nС��10^100^��
+输入格式：每个测试输入包含1个测试用例，即给出自然数n的值。这里保证n小于10^100^。
 
-�����ʽ����һ�������n�ĸ�λ����֮�͵�ÿһλ��ƴ�����ּ���1 �ո񣬵�һ�������һ��ƴ�����ֺ�û�пո�
+输出格式：在一行内输出n的各位数字之和的每一位，拼音数字间有1 空格，但一行中最后一个拼音数字后没有空格。
 
-����������
+输入样例：
 
 1234567890987654321123456789
-���������
+输出样例：
 
 yi san wu
 */
@@ -28,5 +28,26 @@ int main(){
     a = to_string(sum);
     for(i=0 ; i<a.size() ; i++)
         i == a.size()-1 ? cout<<res[a[i]-'0'] : cout<<res[a[i]-'0']+" ";
+    return 0;
+}
+
+
+//他山之玉
+//柳婼 の blog
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int sum = 0;
+    string str[10] =  {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+    for (int i = 0; i < s.length(); i++)
+        sum += (s[i] - '0');
+    string num = to_string(sum);
+    for (int i = 0; i < num.length(); i++) {
+        if (i != 0) cout << " ";
+        cout << str[num[i] - '0'];
+    }
     return 0;
 }
