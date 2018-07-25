a4b2242c2e083d4ecfe97f6d15c3a73007b8f758
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-07-25 12:00:55 +0800

    Tree Traversals Again (25) —— 二叉树的遍历

---
+++ 2086_A.cpp
@@ -0,0 +1,142 @@
+/*
+1086 Tree Traversals Again (25)（25 分）提问
+An inorder binary tree traversal can be implemented in a non-recursive way with a stack. For example, suppose that when a 6-node binary tree (with the keys numbered from 1 to 6) is traversed, the stack operations are: push(1); push(2); push(3); pop(); pop(); push(4); pop(); pop(); push(5); push(6); pop(); pop(). Then a unique binary tree (shown in Figure 1) can be generated from this sequence of operations. Your task is to give the postorder traversal sequence of this tree.
+
+\ Figure 1
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line contains a positive integer N (<=30) which is the total number of nodes in a tree (and hence the nodes are numbered from 1 to N). Then 2N lines follow, each describes a stack operation in the format: "Push X" where X is the index of the node being pushed onto the stack; or "Pop" meaning to pop one node from the stack.
+
+Output Specification:
+
+For each test case, print the postorder traversal sequence of the corresponding tree in one line. A solution is guaranteed to exist. All the numbers must be separated by exactly one space, and there must be no extra space at the end of the line.
+
+Sample Input:
+
+6
+Push 1
+Push 2
+Push 3
+Pop
+Pop
+Push 4
+Pop
+Pop
+Push 5
+Push 6
+Pop
+Pop
+Sample Output:
+
+3 4 2 6 5 1
+*/
+
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+int Res[10000] = {0};
+int z = 0;
+
+void PostT(vector<int>pre,vector<int>in,int root,int start,int ed)
+{
+    if(start > ed)
+        return;
+    int j = start;
+    while(j<ed && in[j]!=pre[root])
+    {
+        j++;
+    }
+    PostT(pre,in,root+1,start,j-1);
+    PostT(pre,in,root+(j-start)+1,j+1,ed);
+    Res[z++] = pre[root];
+}
+
+int main()
+{
+    int N;
+    cin>>N;
+    vector<int>pre;
+    vector<int>in;
+    stack<int>Temp;
+    int i;
+    string CaoZuo;
+    int t;
+    for(i=0 ; i<N*2 ; i++)
+    {
+        cin>>CaoZuo;
+        if(CaoZuo == "Push")
+        {
+            cin>>t;
+            Temp.push(t);
+            pre.push_back(t);
+        }
+        else
+        {
+            t = Temp.top();
+            in.push_back(t);
+            Temp.pop();
+        }
+    }
+    PostT(pre,in,0,0,N-1);
+    for(i=0 ; i<N ; i++)
+    {
+        if(i==0)
+            cout<<Res[i];
+        else
+            cout<<" "<<Res[i];
+    }
+    return 0;
+
+}
+
+
+//他山之玉
+//柳婼
+/*
+题目大意：用栈的形式给出一棵二叉树的建立的顺序，求这棵二叉树的后序遍历
+分析：栈实现的是二叉树的中序遍历（左根右），而每次push入值的顺序是二叉树的前序遍历（根左右），所以该题可以用二叉树前序和中序转后序的方法做~~
+root为当前子树的根结点在前序pre中的下标，start和end为当前子树的最左边和最右边的结点在中序in中的下标。用i找到当前子树的根结点root在中序中的下标然
+后左边和右边就分别为当前根结点root的左子树和右子树。递归实现~
+Update：Github用户littlesevenmo给我发issue提出题目并没有说所有节点的值互不相同。因此，在有多个节点的值相同的情况下，之前的代码会输出错误的结果
+，所以修改后的代码中添加了key作为索引，前中后序中均保存索引值，然后用value存储具体的值，修改后的代码如下：
+*/
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <cstring>
+using namespace std;
+vector<int> pre, in, post,value;
+void postorder(int root, int start, int end) {
+    if (start > end) return;
+    int i = start;
+    while (i < end && in[i] != pre[root]) i++;
+    postorder(root + 1, start, i - 1);
+    postorder(root + 1 + i - start, i + 1, end);
+    post.push_back(pre[root]);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    char str[5];
+    stack<int> s;
+    int key=0;
+    while (~scanf("%s", str)) {
+        if (strlen(str) == 4) {
+            int num;
+            scanf("%d", &num);
+            value.push_back(num);
+            pre.push_back(key);
+            s.push(key++);
+        } else {
+            in.push_back(s.top());
+            s.pop();
+        }
+    }
+    postorder(0, 0, n - 1);
+    printf("%d", value[post[0]]);
+    for (int i = 1; i < n; i++)
+        printf(" %d",value[post[i]]);
+    return 0;
+}
