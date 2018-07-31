a767676496fdd85ab4837cd46f3b04c24e959df7
Haoran Lou <37174250+Pythonboy@users.noreply.github.com>
2018-07-31 10:45:33 +0800

    Update 2106_A.cpp

--- 2106_A.cpp
+++ 2106_A.cpp
@@ -103,3 +103,51 @@ int main()
     printf("%.4f %d",P*pow(1+R,cnt-1),num);
     return 0;
 }
+
+
+
+
+
+
+
+//他山之玉
+//柳婼
+/*
+题目大意：提供一棵树，在树根处货物的价格为p，从根结点开始每往下一层，该层货物价格将会在父亲结点的价格上增加r%。求叶子结点出能获得的最低价格以及能提供最低价格的叶子结点数
+分析：dfs。保存深度的最小值mindepth，以及最小值下该深度的个数minnum。深度优先搜索参数为index和depth，不断遍历index结点的孩子结点，直到当前结点没有孩子结点为止return~~~~
+*/
+#include <cstdio>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<int> v[100005];
+int mindepth = 99999999, minnum = 1;
+void dfs(int index, int depth) {
+    if(mindepth < depth)
+        return ;
+    if(v[index].size() == 0) {
+        if(mindepth == depth)
+            minnum++;
+        else if(mindepth > depth) {
+            mindepth = depth;
+            minnum = 1;
+        }
+    }
+    for(int i = 0; i < v[index].size(); i++)
+        dfs(v[index][i], depth + 1);
+}
+int main() {
+    int n, k, c;
+    double p, r;
+    scanf("%d %lf %lf", &n, &p, &r);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &k);
+        for(int j = 0; j < k; j++) {
+            scanf("%d", &c);
+            v[i].push_back(c);
+        }
+    }
+    dfs(0, 0);
+    printf("%.4f %d", p * pow(1 + r/100, mindepth), minnum);
+    return 0;
+}
